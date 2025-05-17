9be714fde67eb099a3091ae7b827f04073ecca5c
Ali-Samehh <alisameh71010@gmail.com>
2025-05-17 19:36:18 +0300

    deploying 2

--- index.js
+++ index.js
@@ -1,56 +1,65 @@
-const gridArea = 600;
-const squarePerSide = 16;
-let color = 'navy';
+let squarePerSide = 16 ;
+let gridArea = 600 ;
+let color = 'navy'
 
-
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     const sketchArea = document.querySelector('.sketch');
-    sketchArea.style.width = sketchArea.style.height = `${gridArea}px`;
+    sketchArea.style.height = sketchArea.style.width = `${gridArea}px`;
 
-    function createGridCell (squarePerSide) {
-        for (let i = 1; i <= squarePerSide * squarePerSide; i++) {
-            const gridCell = document.createElement('div');
-            gridCell.style.width = gridCell.style.height = `${(gridArea / squarePerSide) - 2}px`;
+    function createGrid(squarePerSide) {
+        squareTotal = squarePerSide * squarePerSide;
+        heightAndWidth = `${(gridArea / squarePerSide) - 2}`;
+        for (let i = 1 ; i <= squareTotal ; i++) {
+            let gridCell = document.createElement('div');
             gridCell.classList.add('cell');
+            gridCell.style.width =  gridCell.style.heigh = `${heightAndWidth}px`;
             sketchArea.appendChild(gridCell);
-            gridCell.addEventListener('mouseover' , setColorDiv );
+            gridCell.addEventListener('mouseover', setColorDiv);
+
         }
     }
-
-    createGridCell(squarePerSide);
-
-    function removeGridCell () {
-        const gridCells = document.querySelectorAll('.cell');
-        gridCells.forEach(cell => {
-            cell.remove();
-        });
-    }
-
-    const sizeBtn = document.querySelector('.size');
+    createGrid(squarePerSide);
 
 
     function createUserGrid () {
-        let userInput = prompt('Enter the number of squares per side (1-100):');
-        if (userInput > 100 || userInput < 1) {
+        let userInput = prompt ('Enter the number of squares per side (1-100):');
+        if (userInput < 0 || userInput > 100) {
             alert('Please enter a number between 1 and 100');
             createUserGrid();
         }
         else {
-            removeGridCell();
-            createGridCell(userInput);
+            removeGrid();
+            createGrid(userInput);
         }
     }
 
-    sizeBtn.addEventListener('click', createUserGrid);
+    sizeBtn = document.querySelector('.size');
+    sizeBtn.addEventListener('click', createUserGrid)
+
+
+    function removeGrid () {
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach(cell => {
+            cell.remove();
+        });
+    }
 
     const clearBtn = document.querySelector('.clear');
 
+    function clearGrid () {
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach (cell => {
+            cell.style.backgroundColor = 'white';
+        })
+    }
+
     clearBtn.addEventListener('click', clearGrid);
 
-    function clearGrid () {
+    const randomBtn = document.querySelector('.random');
 
-        let divs = document.querySelectorAll('.cell');
-        divs.forEach (div => div.style.backgroundColor = 'white');
+
+    function setColor (colorChoice) {
+        color = colorChoice;
     }
 
     function setColorDiv () {
@@ -62,11 +71,5 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    function setColor (colorChoice) {
-        color = colorChoice;    
-    }
-
-    const randomBtn = document.querySelector('.random');
-    randomBtn.addEventListener('click', () => setColor("random"));
-
+    randomBtn.addEventListener('click',() => setColor('random'))
 });
